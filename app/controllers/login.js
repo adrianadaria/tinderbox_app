8283d7a46eee74630d001d80d78c37cbf0eacf30
Konstantin <kostylo@windowslive.com>
2016-10-06 18:58:28 +0200

    added login view, register view, you can switch between them. Also added home page with scrollable view and pagging controll

---
+++ app/controllers/login.js
@@ -0,0 +1,31 @@
+
+var logWin = $.logWin;
+var logView = $.logView;
+var regView = Alloy.createController('register').getView();
+
+boolean: k = false;
+
+function login(e) {
+	Alloy.createController('home').getView().open();
+}
+
+function register(e) {
+    logView.setVisible(false);
+    logWin.add(regView);
+    k = true;
+}
+
+logWin.addEventListener('android:back', function(e){
+
+	if (k == false) {
+		logWin.close();
+	} else {
+		logWin.remove(regView);
+		logView.setVisible(true);
+		k = false;
+	}
+});
+
+logWin.addEventListener("close", function() {
+	$.destroy();
+});
