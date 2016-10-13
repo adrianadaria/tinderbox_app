995234ccabb7ba354610bec427b34f43b087180b
Konstantin <kostylo@windowslive.com>
2016-10-13 10:05:10 +0200

    kinda final version ATM

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,2 +1,70 @@
 // Arguments passed into this controller can be accessed via the `$.args` object directly or:
 var args = $.args;
+
+now();
+
+function now(){
+	
+	// Keep a global reference of the available pages
+	var numberOfPages = $.messagesMenu.getViews().length;
+	var numberOfPages2 = $.messageMenu2.getViews().length;
+	
+	var pages = []; // without this, the current page won't work on future references of the module
+	var pages2 = [];
+		// Store a reference to this view
+	pages.push($.pView1, $.pView2, $.pView3);
+	pages2.push($.p2View1, $.p2View2, $.p2View3, $.p2View4);
+	
+	// Mark the initial selected page
+	pages[$.messagesMenu.getCurrentPage()].setOpacity(1);
+	pages[$.messageMenu2.getCurrentPage()].setOpacity(1);
+	
+	// Callbacks
+	onScroll = function(event){
+		// Go through each and reset it's opacity
+		for (var i = 0; i < numberOfPages; i++) {
+			pages[i].setOpacity(0.5);
+		}
+		// Bump the opacity of the new current page
+		var currPage = event.currentPage;
+		pages[currPage].setOpacity(1);
+		
+	};
+	
+	onScroll2 = function(event){
+		// Go through each and reset it's opacity
+		for (var i = 0; i < numberOfPages2; i++) {
+			pages2[i].setOpacity(0.5);
+		}
+		// Bump the opacity of the new current page
+		var currPage = event.currentPage;
+		pages2[currPage].setOpacity(1);
+		
+	};
+	
+	onPostLayout = function(event) {
+	// Go through each and reset it's opacity
+	for(var i = 0; i < numberOfPages; i++) {
+		pages[i].setOpacity(0.5);
+	}
+	// Bump the opacity of the new current page
+	pages[$.messagesMenu.currentPage].setOpacity(1);
+	
+};
+
+	onPostLayout2 = function(event) {
+	// Go through each and reset it's opacity
+	for(var i = 0; i < numberOfPages2; i++) {
+		pages2[i].setOpacity(0.5);
+	}
+	// Bump the opacity of the new current page
+	pages2[$.messageMenu2.currentPage].setOpacity(1);
+	
+};
+	
+	// Attach the scroll event to this scrollableView, so we know when to update things
+	$.messagesMenu.addEventListener("scroll", onScroll);
+	$.messagesMenu.addEventListener("postlayout", onPostLayout);
+	$.messageMenu2.addEventListener("scroll", onScroll2);
+	$.messageMenu2.addEventListener("postlayout", onPostLayout2);
+};
