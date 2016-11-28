3e0ae514ed15172a15ee194a403a0a715434249c
verrr <andreiverdes@yahoo.com>
2016-11-28 16:22:55 +0100

    register with functionality

--- tinderv2/app/controllers/register.js
+++ tinderv2/app/controllers/register.js
@@ -1,10 +1,42 @@
 // Arguments passed into this controller can be accessed via the `$.args` object directly or:
 var args = $.args;
 
-function createUser(e) {
+function createUser() {
 	
-}
+	var name = $.fullname.value;
+	var phone = $.phone.value;
+	var email = $.email.value;
+	var password = $.password.value;
+	var passwordCheck = $.passwordCheck.value;
+	
+	var params = {
+			"userName": name,
+			"userPass": password,
+			"phone": phone,
+			"email": email
+		};
+		
+	var xhr = Ti.Network.createHTTPClient();
+	var url = "http://kostylo.dk/projects/api/createUser";
+	xhr.open("POST",url);
+	
+	alert("clicked");
+	
+	xhr.onload = function() {
+		
+		Alloy.createController('home').getView().open();
+		
+	};
+	
+	xhr.onerror = function (){
 
+		alert("error");
+		
+	};
+	
+	xhr.send(params);
+
+}
 
 function uploadImage(e) {
 	
@@ -14,3 +46,17 @@ function uploadImage(e) {
 function outputState(){
     Ti.API.info('Switch value: ' + $.basicSwitch.value);
 }
+
+$.regWin.addEventListener("close", function() {
+	$.destroy();
+});
+
+$.regWin.addEventListener('open',function(evt){
+	if (OS_ANDROID){
+		var activity=evt.source.getActivity();
+		var actionbar=activity.actionBar;
+		actionbar.hide();
+		
+	}
+});
+//
