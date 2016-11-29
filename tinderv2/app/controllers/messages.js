e2ec0e888eac09b365bf339a2a196e145d4539a9
Joycruiser <westside_gesh@abv.bg>
2016-11-29 16:38:07 +0100

    up

--- tinderv2/app/controllers/messages.js
+++ tinderv2/app/controllers/messages.js
@@ -1,86 +1,46 @@
 // Arguments passed into this controller can be accessed via the `$.args` object directly or:
 var args = $.args;
 var collection = Alloy.Collections.messages;
+collection.fetch();
 
-collection.fetch({
-	success: function() {
-		_.each(collection.models, function(element, index, list){
-			
-			element.attributes.sid = element.sid;	
-			element.attributes.topic = element.attributes.topic;
-			element.attributes.content = element.attributes.content;
-			
-			
-		});
-	},
-	error : function() {
-		Ti.API.error("this is not good");
+function transform(model) {
+	
+	var fontsize = (Titanium.Platform.displayCaps.platformHeight*2)/100;
+
+	var PcObject = model.toJSON();
+	console.log(PcObject);
+	
+	var label = Ti.UI.createLabel({
+  	text: PcObject.topic,
+	top: 10,
+	backgroundColor: '#8ac6b8',
+	color: 'black',
+	width: "80%",
+	left: "10%",
+	right: "10%",
+	textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+	height: 35,
+	font: {
+		fontSize: fontsize
 	}
-});
-
-
-
-function selectRow(e) {
- var rowId = e.rowData.rowId;
- var rowIheading = e.rowData.rowIheading;
- var rowDescription = e.rowData.rowDescription;
- //alert(rowDescription);
- 	var detailController = Alloy.createController("detail", {
-		rowIheading : rowIheading,
-		rowDescription : rowDescription
 	});
-	openAsModal(detailController.getView());
-}
-
-function openAsModal(_view) {
-	if (OS_IOS) {
-		var navWindow = Titanium.UI.iOS.createNavigationWindow({
-			window : _view
-		});
-
-		_view.navWindow = navWindow;
-		navWindow.open({
-			modal : true
-		});
-	} else {
-		_view.open({
-			modal : true
-		});
+	
+	var label2 = Ti.UI.createLabel({
+  	text: PcObject.content,
+	top: 0,
+	backgroundColor: 'white',
+	color: 'black',
+	width: "80%",
+	left: "10%",
+	right: "10%",
+	textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+	height: 35,
+	font: {
+		fontSize: fontsize
 	}
-}
-
-/**
- * Callback for Android OptionsMenu
- */
-function onCreateOptionsMenu(e) {
-  
-  // additional overflow menu item
-  e.menu.add({
-    title : "Main option",
-    showAsAction : Ti.Android.SHOW_AS_ACTION_NEVER
-  });
+	});
+	
+	$.MessageCon.add(label);
+	$.MessageCon.add(label2);
 
 }
-
-/**
- * Cleans up the controller
- * 
- * http://www.tidev.io/2014/09/18/cleaning-up-alloy-controllers/
- */
-function cleanup() {
-  $.off();
-}
-
-/**
- * Initializes the controller
- */
-function init() {
-  
-  $.on('createOptionsMenu', onCreateOptionsMenu);
-
-}
-
-// PUBLIC
-exports.id = 'messages';
-exports.cleanup = cleanup;
-exports.init = init;
